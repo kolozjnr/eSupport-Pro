721fb2b4a11b3ace638109804f8b808b0a3624e8
Barnabas KolozJnr <67397553+kolozjnr@users.noreply.github.com>
2025-07-27 23:54:48 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,21 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css',
-                    'resources/js/app.js',
-                    'resources/landing/css/style.css',     // landing page Tailwind CSS
-                    'resources/landing/css/animate.css',     // landing page Tailwind CSS
-                    'resources/landing/js/index.js',     // landing page Tailwind CSS
-                    'resources/landing/js/typewriter.js',     // landing page Tailwind CSS
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/landing/css/style.css',
+                'resources/landing/css/animate.css',
+                'resources/landing/js/index.js',
+                'resources/landing/js/typewriter.js',
             ],
-            //refresh: true,
-              build: {
+            refresh: true,
+        }),
+    ],
+    build: {
         manifest: true,
         outDir: 'public/build',
         emptyOutDir: true,
     },
-        }),
-    ],
+    base: '/build/', // This ensures assets are served from /build/ path
 });
