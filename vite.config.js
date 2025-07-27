fb3aa0f0333d2f05a8ccac721306bc1b1d5d2345
Barnabas KolozJnr <67397553+kolozjnr@users.noreply.github.com>
2025-07-28 00:01:46 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,17 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
+    base: '/build/',
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
                 'resources/landing/css/style.css',
                 'resources/landing/css/animate.css',
                 'resources/landing/js/index.js',
-                'resources/landing/js/typewriter.js',
+                'resources/landing/js/typewriter.js'
             ],
             refresh: true,
         }),
     ],
-    build: {
-        manifest: true,
-        outDir: 'public/build',
-        emptyOutDir: true,
-    },
-    base: '/build/', // This ensures assets are served from /build/ path
-});
+})
