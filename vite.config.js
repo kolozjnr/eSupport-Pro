ca486a364812b1a5c1942959e0e29a74569c95b0
Barnabas KolozJnr <67397553+kolozjnr@users.noreply.github.com>
2025-07-28 00:11:26 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
-    base: '/build/',
+    base: '/esupportpro/public/build/',
     plugins: [
         laravel({
             input: [
