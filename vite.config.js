ae148e44b8efd41b32d58c9bc160246ad222d55b
Barnabas KolozJnr <67397553+kolozjnr@users.noreply.github.com>
2025-07-28 11:03:54 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/landing/css/style.css',
+                'resources/css/app.css',                         // dashboard
+                'resources/js/app.js',
+                'resources/landing/css/style.css',              // landing
                 'resources/landing/css/animate.css',
                 'resources/landing/js/index.js',
                 'resources/landing/js/typewriter.js'
@@ -16,6 +18,30 @@ export default defineConfig({
     build: {
         outDir: 'public/build',
     },
-    // Only use base for production builds
-    base: process.env.NODE_ENV === 'production' ? '/esupportpro/public/build/' : '/',
-})
+    base: process.env.NODE_ENV === 'production'
+        ? '/esupportpro/public/build/'
+        : '/',
+});
+
+
+// import { defineConfig } from 'vite'
+// import laravel from 'laravel-vite-plugin'
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: [
+//                 'resources/landing/css/style.css',
+//                 'resources/landing/css/animate.css',
+//                 'resources/landing/js/index.js',
+//                 'resources/landing/js/typewriter.js'
+//             ],
+//             refresh: true,
+//         }),
+//     ],
+//     build: {
+//         outDir: 'public/build',
+//     },
+//     // Only use base for production builds
+//     base: process.env.NODE_ENV === 'production' ? '/esupportpro/public/build/' : '/',
+// })
