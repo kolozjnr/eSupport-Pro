6778d099fd49e6cf795106f2c3382426c99c8969
Barnabas KolozJnr <67397553+kolozjnr@users.noreply.github.com>
2025-07-31 19:04:35 +0100

    admin dashboard

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -745,12 +745,12 @@
                     class="relative z-30 mb-14 h-[490px] max-w-[600px] lg:mb-0"
                   >
                     <div class="absolute left-0 top-0">
-                      <img src="{{asset('landing/images/about/image-1.jpg')}}" alt="about-image" />
+                      <img src="{{asset('landing/images/about/image-6.jpg')}}" alt="about-image" />
                     </div>
                     <div
                       class="absolute right-0 top-1/2 z-10 -translate-y-1/2"
                     >
-                      <img src="{{asset('landing/images/about/image-2.jpg')}}" alt="about-image" />
+                      <img src="{{asset('landing/images/about/image-4.jpg')}}" alt="about-image" />
                       <div
                         class="absolute -left-5 -top-5 -z-10 h-full w-full border border-primary border-opacity-10 bg-primary bg-opacity-5 backdrop-blur-[6px] dark:border-white dark:border-opacity-10 dark:bg-white dark:bg-opacity-10"
                       ></div>
@@ -825,12 +825,12 @@
                     class="relative z-30 mb-14 h-[490px] max-w-[600px] lg:mb-0"
                   >
                     <div class="absolute left-0 top-0">
-                      <img src="{{asset('landing/images/about/image-1.jpg')}}" alt="about-image" />
+                      <img src="{{asset('landing/images/about/image-6.jpg')}}" alt="about-image" />
                     </div>
                     <div
                       class="absolute right-0 top-1/2 z-10 -translate-y-1/2"
                     >
-                      <img src="{{ asset('landing/images/about/image-2.jpg')}}" alt="about-image" />
+                      <img src="{{ asset('landing/images/about/image-4.jpg')}}" alt="about-image" />
                       <div
                         class="absolute -left-5 -top-5 -z-10 h-full w-full border border-primary border-opacity-10 bg-primary bg-opacity-5 backdrop-blur-[6px] dark:border-white dark:border-opacity-10 dark:bg-white dark:bg-opacity-10"
                       ></div>
@@ -905,12 +905,12 @@
                     class="relative z-30 mb-14 h-[490px] max-w-[600px] lg:mb-0"
                   >
                     <div class="absolute left-0 top-0">
-                      <img src="{{asset('landing/images/about/image-1.jpg')}}" alt="about-image" />
+                      <img src="{{asset('landing/images/about/image-6.jpg')}}" alt="about-image" />
                     </div>
                     <div
                       class="absolute right-0 top-1/2 z-10 -translate-y-1/2"
                     >
-                      <img src="{{ asset('landing/images/about/image-2.jpg')}}" alt="about-image" />
+                      <img src="{{ asset('landing/images/about/image-4.jpg')}}" alt="about-image" />
                       <div
                         class="absolute -left-5 -top-5 -z-10 h-full w-full border border-primary border-opacity-10 bg-primary bg-opacity-5 backdrop-blur-[6px] dark:border-white dark:border-opacity-10 dark:bg-white dark:bg-opacity-10"
                       ></div>
